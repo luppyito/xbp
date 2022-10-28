890612cbb0f09c625e7a9221764dcbc306373bed
luppyito <r202201994rg@jindai.jp>
2022-10-28 10:52:08 +0900

    1028

---
+++ de12/python/game.py
@@ -0,0 +1,55 @@
+def if_test(num):
+   if num == 2:
+      print('T')
+   else:
+      print('F')
+2
+print('1働き蜂の性別は？　1,メス 2,オス 3,両方')
+num = int(input('何番だと思う?  '))
+if_test(num)
+def if_test(num):
+   if num == 2:
+      print('T')
+   else:
+      print('F')
+3
+print('2 お酢に卵を殻ごといれると卵はどうなるでしょうか？ 1.透明な卵になる 2.硬い石のような卵になる 3.卵が溶けてなくなる ')
+num = int(input('何番だと思う?'))
+if_test(num)
+def if_test(num):
+   if num == 1:
+      print('T')
+   else:
+      print('F')
+
+4
+print('3 シロクマ（ホッキョクグマ）の肌の色は何色でしょうか？ 1.白色 2.肌色 3.黒色 ')
+num = int(input('何番だと思う?'))
+if_test(num)
+def if_test(num):
+   if num == 3:
+      print('T')
+   else:
+      print('F')
+
+5
+print('4 アニメの作品数は？ 1.175,655 2.154,710 3.61,172 ')
+num = int(input('何番だと思う?'))
+if_test(num)
+def if_test(num):
+   if num == 3:
+      print('T')
+   else:
+      print('F')
+
+6
+2
+print('5  次の式の答えはなに？　3.14159×7.55052+2.44948×2.23606+0.90553×2.44948=？　1,31.4159　2,50　3,34.2711　')
+num = int(input('何番だと思う?  '))
+if_test(num)
+def if_test(num):
+   if num == 3:
+      print('T')
+   else:
+      print('F')
+
