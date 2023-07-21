5b5bfd426571128272899ff4eb2f15253206fa1d
luppyito <r202201994rg@jindai.jp>
2023-07-21 15:07:39 +0900

    721

--- de34/circle.js
+++
@@ -1,7 +0,0 @@
-const circle = document.querySelector('.circle');
-        const text = circle.innerHTML;// Note I am being lazy here and assuming the string has no unwanted whitespace
-        circle.innerHTML = '';        
-        circle.style.setProperty('--numchs', text.length);
-        for ( let i = 0; i < text.length; i++ ) {
-          circle.innerHTML = circle.innerHTML + '<div class="char" style="--char-index: ' + i + ';">' + text.charAt(i) + '</div>';
-        }
